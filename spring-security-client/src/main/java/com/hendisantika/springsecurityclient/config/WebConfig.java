2210ba793cc017d6f3aaf5944093dc2261626744
Hendi Santika <hendisantika@gmail.com>
2020-02-05 06:55:52 +0700

    #7 - Add WebConfig

---
+++ spring-security-client/src/main/java/com/hendisantika/springsecurityclient/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springsecurityclient.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-client
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/02/20
+ * Time: 06.55
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
