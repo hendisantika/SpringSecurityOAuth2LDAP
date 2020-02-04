f1519b779bcd36f29ac5dc72415483563086b97a
Hendi Santika <hendisantika@gmail.com>
2020-02-05 06:56:48 +0700

    #11 - Add RequestContextListener in WebConfig

--- spring-security-client/src/main/java/com/hendisantika/springsecurityclient/config/WebConfig.java
+++ spring-security-client/src/main/java/com/hendisantika/springsecurityclient/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.hendisantika.springsecurityclient.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -39,5 +41,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
     }
 }
