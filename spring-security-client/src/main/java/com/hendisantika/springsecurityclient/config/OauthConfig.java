917043eb0ded6680b7c93852644a36f55a5ee308
Hendi Santika <hendisantika@gmail.com>
2020-02-05 06:55:01 +0700

    #6 - Override configure in OauthConfig

--- spring-security-client/src/main/java/com/hendisantika/springsecurityclient/config/OauthConfig.java
+++ spring-security-client/src/main/java/com/hendisantika/springsecurityclient/config/OauthConfig.java
@@ -2,6 +2,7 @@ package com.hendisantika.springsecurityclient.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -16,6 +17,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableOAuth2Sso
 @Configuration
 public class OauthConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
 
-
+    }
 }
