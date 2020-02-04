cca22ac9404a7fd2030ea33376b008d55ccaa5cf
Hendi Santika <hendisantika@gmail.com>
2020-02-05 06:56:18 +0700

    #9 - Add addViewControllers in WebConfig

--- spring-security-client/src/main/java/com/hendisantika/springsecurityclient/config/WebConfig.java
+++ spring-security-client/src/main/java/com/hendisantika/springsecurityclient/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.hendisantika.springsecurityclient.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -20,5 +21,17 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+
+        registry.addViewController("/")
+                .setViewName("forward:/index");
+
+        registry.addViewController("/index");
+        registry.addViewController("/secure");
+
     }
 }
