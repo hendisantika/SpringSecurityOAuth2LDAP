a2565be68169fcfd24d61273acd4fa19ed1838e6
Hendi Santika <hendisantika@gmail.com>
2020-02-05 07:08:22 +0700

    #24 - Add user & index API in HelloController in spring-security-oauth2-server-ldap

--- spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/controller/HelloController.java
+++ spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.hendisantika.springsecurityoauth2serverldap.controller;
 
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,5 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rest/api/")
 public class HelloController {
+    @GetMapping("/principal")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
 
 }
