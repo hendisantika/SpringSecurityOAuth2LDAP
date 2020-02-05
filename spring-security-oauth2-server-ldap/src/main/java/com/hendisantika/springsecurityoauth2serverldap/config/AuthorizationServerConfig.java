f13498bef43608d532511efaabe5072dbf7b57e1
Hendi Santika <hendisantika@gmail.com>
2020-02-05 07:05:49 +0700

    #20 - Add configure in spring-security-oauth2-server-ldap

--- spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/config/AuthorizationServerConfig.java
+++ spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.hendisantika.springsecurityoauth2serverldap.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
@@ -28,6 +29,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
         security.tokenKeyAccess("permitAll()")
                 .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("ClientId")
+                .secret("secret")
+                .authorizedGrantTypes("authorization_code")
+                .scopes("user_info")
+                .autoApprove(true);
     }
 
 }
