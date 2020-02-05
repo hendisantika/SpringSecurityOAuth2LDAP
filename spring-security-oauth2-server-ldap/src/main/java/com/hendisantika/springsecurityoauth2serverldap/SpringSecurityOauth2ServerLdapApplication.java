1e47e0692c4df618f374308fcd0cbe8929746a5d
Hendi Santika <hendisantika@gmail.com>
2020-02-05 07:00:34 +0700

    #15 - Initial commit for spring-security-oauth2-server-ldap

---
+++ spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/SpringSecurityOauth2ServerLdapApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springsecurityoauth2serverldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2ServerLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2ServerLdapApplication.class, args);
+    }
+
+}
