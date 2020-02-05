76a8e9b55645c11c9d85c65771169bbb44c8b8f9
Hendi Santika <hendisantika@gmail.com>
2020-02-05 07:05:36 +0700

    #19 - Add configure in spring-security-oauth2-server-ldap

--- spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/config/AuthorizationServerConfig.java
+++ spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +22,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+
+        security.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
 
 }
