8f2f0fee20f901abda3dd34af1b2878a667efcc3
Hendi Santika <hendisantika@gmail.com>
2020-02-05 07:06:36 +0700

    #22 - Add SpringBootServletInitializer in spring-security-oauth2-server-ldap

--- spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/SpringSecurityOauth2ServerLdapApplication.java
+++ spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/SpringSecurityOauth2ServerLdapApplication.java
@@ -2,9 +2,16 @@ package com.hendisantika.springsecurityoauth2serverldap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringSecurityOauth2ServerLdapApplication {
+public class SpringSecurityOauth2ServerLdapApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringSecurityOauth2ServerLdapApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityOauth2ServerLdapApplication.class, args);
