214086839c9d12ed9fa383e72838ccfbcf510089
Hendi Santika <hendisantika@gmail.com>
2020-02-05 07:05:21 +0700

    #18 - Add AuthorizationServerConfig in spring-security-oauth2-server-ldap

---
+++ spring-security-oauth2-server-ldap/src/main/java/com/hendisantika/springsecurityoauth2serverldap/config/AuthorizationServerConfig.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springsecurityoauth2serverldap.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-oauth2-server-ldap
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/02/20
+ * Time: 07.02
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+}
